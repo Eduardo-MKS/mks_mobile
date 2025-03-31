dc4c9bf3f944e619d4ba4645060d847bea240fe7
Eduardo Decussi <eduardo@mkssistemas.com.br>
2025-03-31 11:57:54 -0300

    Fix Created new desing filters and add new filters

--- components/DrawerNavigation.tsx
+++ components/DrawerNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import {
   View,
@@ -7,12 +7,14 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  Keyboard,
 } from "react-native";
 import { Input } from "react-native-elements";
 import HomeScreen from "../screens/HomeScreen";
 import Slider from "@react-native-community/slider";
-//import DropDownPicker from "react-native-dropdown-picker";
 import axios from "axios";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const Drawer = createDrawerNavigator();
 const ESTACOES_API = "https://api-dcsc.mks-unifique.ddns.net/api/estacoes";
@@ -25,8 +27,35 @@ export function MyDrawer() {
   const [openParametro, setOpenParametro] = useState(false);
   const [selectedParametro, setSelectedParametro] = useState(null);
   const [stations, setStations] = useState([]);
+  const [openTipos, setOpenTipos] = useState(false);
+  const [selectedTipos, setSelectedTipos] = useState(null);
+  const [isOpenInfo, setIsOpenInfo] = useState(false);
+  const [isOpenRadar, setIsOpenRadar] = useState(false);
+  const searchInputRef = useRef(null);
 
-  // Busca as estações da API
+  const [valoresPreDefinidos, setValoresPreDefinidos] = useState([
+    { label: "Chuva Acumulada (mm)", value: "12,5 mm" },
+    { label: "Chuva Instantanea (mm)", value: "15,5 mm" },
+    { label: "Chuva Deslizamento (mm)", value: "2,5 mm" },
+    { label: "Rio m", value: "8,2 m" },
+    { label: "Temperatura", value: "25 C°" },
+  ]);
+
+  const [tipos, setTipos] = useState([
+    { label: "5 minutos", value: "5 minutos" },
+    { label: "10 minutos", value: "10 minutos" },
+    { label: "15 minutos", value: "15 minutos" },
+    { label: "30 minutos", value: "30 minutos" },
+  ]);
+
+  const [parametros, setParametros] = useState([
+    { label: "Chuva Acumulada (mm)", value: "Chuva Acumulada (mm)" },
+    { label: "Chuva Instantanea (mm)", value: "Chuva Instantanea (mm)" },
+    { label: "Chuva Deslizamento (mm)", value: "Chuva Deslizamento (mm)" },
+    { label: "Rio m", value: "Rio m" },
+    { label: "Temperatura", value: "Temperatura" },
+  ]);
+
   useEffect(() => {
     const fetchStations = async () => {
       try {
@@ -34,7 +63,7 @@ export function MyDrawer() {
         const stationsData = response.data.data;
 
         const stationsArray = Object.keys(stationsData).map((key) => ({
-          id: key, // Usando a chave como ID
+          id: key,
           title: stationsData[key].nome,
           latitude: stationsData[key].latitude,
           longitude: stationsData[key].longitude,
@@ -62,6 +91,7 @@ export function MyDrawer() {
     setSelectedStation(station);
     setSearchText(station.title);
     setSuggestions([]);
+    Keyboard.dismiss();
   };
 
   const applySelection = (navigation) => {
@@ -73,6 +103,18 @@ export function MyDrawer() {
     setSearchText("");
     setSuggestions([]);
     setSelectedParametro(null);
+    setSelectedTipos(null);
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  };
+
+  const toggleOpenInfo = () => {
+    setIsOpenInfo(!isOpenInfo);
+  };
+
+  const toggleOpenRadar = () => {
+    setIsOpenRadar(!isOpenRadar);
   };
 
   return (
@@ -82,6 +124,7 @@ export function MyDrawer() {
         <View style={styles.drawerContent}>
           <View style={styles.contentContainer}>
             <Input
+              ref={searchInputRef}
               placeholder="Buscar Estação"
               value={searchText}
               onChangeText={handleSearch}
@@ -96,21 +139,97 @@ export function MyDrawer() {
                     <Text style={styles.suggestionItem}>{item.title}</Text>
                   </TouchableOpacity>
                 )}
+                style={styles.suggestionsList}
               />
             )}
           </View>
 
-          <View style={styles.opacityContainer}>
-            <Text style={styles.opacityText}>
-              Opacidade: {Math.round(opacity * 100)}%
-            </Text>
-            <Slider
-              value={opacity}
-              onValueChange={setOpacity}
-              minimumValue={0}
-              maximumValue={1}
-              step={0.01}
-            />
+          <View style={styles.infoContainer}>
+            <TouchableOpacity
+              style={styles.infoHeader}
+              onPress={toggleOpenInfo}
+            >
+              <Icon
+                name="information-outline"
+                size={20}
+                color="#DDA853"
+                style={styles.infoIcon}
+              />
+              <Text style={styles.radarTitle}>Informações</Text>
+            </TouchableOpacity>
+            {isOpenInfo && (
+              <View style={styles.infoContainerDropdown}>
+                <DropDownPicker
+                  open={openParametro}
+                  value={selectedParametro}
+                  items={parametros}
+                  setOpen={setOpenParametro}
+                  setValue={setSelectedParametro}
+                  setItems={setParametros}
+                  closeAfterSelecting={true}
+                  placeholder="Parâmetro"
+                  zIndex={3000}
+                  onChangeValue={(value) => {
+                    const preDefinido = valoresPreDefinidos.find(
+                      (item) => item.label === value
+                    );
+                    console.log(preDefinido);
+                  }}
+                />
+                <Input
+                  value={
+                    valoresPreDefinidos.find(
+                      (item) => item.label === selectedParametro
+                    )?.value
+                  }
+                  clearButtonMode="always"
+                  placeholder="Parâmetro"
+                  style={styles.input}
+                ></Input>
+                <DropDownPicker
+                  open={openTipos}
+                  value={selectedTipos}
+                  items={tipos}
+                  setOpen={setOpenTipos}
+                  setValue={setSelectedTipos}
+                  setItems={setTipos}
+                  closeAfterSelecting={true}
+                  placeholder="Tipos"
+                  style={styles.dropdownTipos}
+                  zIndex={1000}
+                />
+              </View>
+            )}
+          </View>
+
+          <View style={styles.rainContainer}>
+            <TouchableOpacity
+              style={styles.radarHeader}
+              onPress={toggleOpenRadar}
+            >
+              <Icon
+                name="radar"
+                size={20}
+                color="#DDA853"
+                style={styles.radarIcon}
+              />
+              <Text style={styles.radarTitle}>Radar</Text>
+            </TouchableOpacity>
+
+            {isOpenRadar && (
+              <View style={styles.opacityContainer}>
+                <Text style={styles.opacityText}>
+                  Opacidade: {Math.round(opacity * 100)}%
+                </Text>
+                <Slider
+                  value={opacity}
+                  onValueChange={setOpacity}
+                  minimumValue={0}
+                  maximumValue={1}
+                  step={0.01}
+                />
+              </View>
+            )}
           </View>
 
           <View style={styles.buttonContainer}>
@@ -151,6 +270,7 @@ export function MyDrawer() {
     </Drawer.Navigator>
   );
 }
+
 const styles = StyleSheet.create({
   drawerContent: {
     flex: 1,
@@ -158,15 +278,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#143D60",
   },
-  contentContainer: { marginBottom: 20 },
-  footerContainer: { marginBottom: 5, alignItems: "center" },
+
+  contentContainer: {
+    marginBottom: 20,
+  },
+
+  footerContainer: {
+    marginBottom: 5,
+    alignItems: "center",
+  },
+
   buttonContainer: {
+    marginTop: 10,
     flexDirection: "row",
     justifyContent: "space-around",
-    marginBottom: 10,
+    marginBottom: 20,
     color: "#fff",
     backgroundColor: "#143D60",
   },
+
   searchInput: {
     borderWidth: 1,
     borderColor: "#ccc",
@@ -174,22 +304,82 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     color: "#fff",
   },
-  drawerFooter: { alignItems: "center" },
-  profilePhoto: { width: 200, height: 100, borderRadius: 50, marginBottom: 20 },
+
+  drawerFooter: {
+    alignItems: "center",
+  },
+
+  profilePhoto: {
+    width: 200,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+
   suggestionItem: {
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: "#fff",
-    color: "#fff",
   },
-  opacityContainer: {
-    marginBottom: 20,
-    backgroundColor: "#fff",
+
+  infoContainer: {
     padding: 10,
-    borderRadius: 8,
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+    margin: 10,
   },
-  buttonTextAplicar: { color: "#fff", textAlign: "center", fontSize: 16 },
-  opacityText: { color: "#000" },
+
+  infoHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+  },
+
+  rainContainer: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+    margin: 10,
+  },
+
+  radarHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderRadius: 5,
+  },
+
+  infoIcon: {
+    marginRight: 10,
+  },
+
+  radarIcon: {
+    marginRight: 10,
+  },
+
+  radarTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  opacityContainer: {
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  buttonTextAplicar: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 16,
+  },
+
+  opacityText: {
+    color: "#000",
+  },
+
   dropInfos: {
     alignItems: "center",
     marginBottom: 10,
@@ -199,18 +389,15 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 180,
   },
-  dropdown: {
-    marginBottom: 16,
-    borderColor: "#ccc",
-    width: "100%",
+
+  infoContainerDropdown: {
+    padding: 10,
+    borderRadius: 5,
   },
-  dropdownContainer: {
-    width: "100%",
-  },
+
   input: {
-    borderWidth: 1,
     borderColor: "#000",
-    color: "#fff",
+    color: "#000",
     fontStyle: "italic",
     borderRadius: 5,
     padding: 10,
@@ -223,11 +410,27 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
   },
+
   buttonLimpar: {
     backgroundColor: "#DDA853",
     padding: 10,
     borderRadius: 5,
   },
-  buttonTextLimpar: { color: "#fff", textAlign: "center", fontSize: 16 },
-  textFooter: { color: "#fff" },
+
+  buttonTextLimpar: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 16,
+  },
+
+  textFooter: {
+    color: "#fff",
+  },
+
+  suggestionsList: {
+    maxHeight: 200,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    marginTop: 5,
+  },
 });
